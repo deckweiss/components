3a2350260264a0084b376b9e73f8231d761d4650
dominikwagner <dominik.wagner@deckweiss.at>
2025-10-10 11:46:21 +0200

    feat: update slider stories with new default values and add multiple value example

--- src/stories/slider.stories.svelte
+++ src/stories/slider.stories.svelte
@@ -9,4 +9,6 @@
 	});
 </script>
 
-<Story name="Slider" args={{ value: [0], min: 0, max: 10, step: 1 }} />
+<Story name="Slider" args={{ value: [5], min: 0, max: 10, step: 1 }}></Story>
+
+<Story name="Multiple" args={{ value: [2, 8], min: 0, max: 10, step: 1 }}></Story>
