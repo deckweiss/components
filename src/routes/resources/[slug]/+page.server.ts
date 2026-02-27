56d6a381593141207864279da33473f121e7eb9f
Thomas Dallinger <thomas.dallinger@deckweiss.at>
2026-02-27 09:53:51 +0100

    fix: build resource slug

--- src/routes/resources/[slug]/+page.server.ts
+++ src/routes/resources/[slug]/+page.server.ts
@@ -1,4 +1,10 @@
-import { loadResourceData } from "$lib/resources";
+import { loadResourceData, resourcesData } from "$lib/resources";
+
+export const prerender = true;
+
+export function entries() {
+	return resourcesData.map((r) => ({ slug: r.slug }));
+}
 
 export async function load({ params }) {
 	return loadResourceData(params.slug);
